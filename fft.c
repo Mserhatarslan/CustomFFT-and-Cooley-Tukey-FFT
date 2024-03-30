2db6dc01dcdfb78a1a5eca577b2347ed1a17ad10
Mehmet Serhat Arslan <63358327+Mserhatarslan@users.noreply.github.com>
2024-03-30 17:02:49 +0300

    Update fft.c

--- fft.c
+++ fft.c
@@ -3,9 +3,10 @@
 #include <math.h>
 #include <assert.h>
 #include <stdbool.h>
+#include <time.h>
 
 #define M_PI 3.14159265358979323846
-#define FFT_SIZE 8 
+#define FFT_SIZE 8192 
 
 typedef double complex cplx;
 
@@ -23,10 +24,9 @@ void performFFT(const cplx fft_in[], cplx fft_out[])
     assert(!isEmpty);
 
     for (int i = 0; i < FFT_SIZE; i++) {
+
         fft_out[i] = 0;
-
         for (int j = 0; j < FFT_SIZE; j++) {
-
             fft_out[i] += fft_in[j] * cexp(-I * 2 * M_PI * i * j / FFT_SIZE);
         }
     }
@@ -34,17 +34,21 @@ void performFFT(const cplx fft_in[], cplx fft_out[])
 
 int main(void)
 {
-    const cplx fft_in[FFT_SIZE] = { 1.0 + 0.0 * I, 2.0 + 0.0 * I , 3.0 + 0.0 * I, 4.0 + 0.0 * I, 
-                                    5.0 + 0.0 * I, 6.0 + 0.0 * I , 7.0 + 0.0 * I, 8.0 + 0.0 * I };
-    
+    const cplx fft_in[FFT_SIZE] = { 1.0 + 0.0 * I, 2.0 + 0.0 * I};
+    clock_t start, end;
+    double cpu_time_used;
+
     cplx fft_out[FFT_SIZE]; 
-    
+      start = clock(); // Başlangıç zamanını kaydet
     performFFT(fft_in, fft_out); 
+     end = clock(); // Bitiş zamanını kaydet
+
     printf(" FFT Results:\n");
     
     for (int i = 0; i < FFT_SIZE; i++) {
         printf("output[%d] = %.2lf + %.2lfi\n", i, creal(fft_out[i]), cimag(fft_out[i]));
     }
-
+    cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
+    printf("Execution time = %lf seconds\n", cpu_time_used); // İşlem süresini hesapla ve yazdır
     return 0; 
 }
